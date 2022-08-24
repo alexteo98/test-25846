deb4c7f72fa8e92af1b288f3d4fa88c88b59759f
alexteo98 <alexteo98@gmail.com>
2022-08-25 03:24:00 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// Import routes
+let apiRoutes = require("./api-routes")
+// Use Api routes in the App
+app.use('/api', apiRoutes)
