1a794e27a1535337dc9e2eb810903b9661682dbc
alexteo98 <alexteo98@gmail.com>
2022-08-25 15:26:17 +0800

    deploy http get post put delete

--- api-routes.js
+++ api-routes.js
@@ -4,19 +4,32 @@ let router = require('express').Router();
 // Set default API response
 router.get('/', function (req, res) {
     res.status(200)
-    json({
-        status: 'API is Working',
+        .json({
         message: 'Sample GET request'
     });
 });
 
 router.post('/', function (req, res) {
     res.status(200)
-    .json({
-        status: 'API is Working',
+        .json({
         message: 'Sample POST request'
     });
 });
+
+router.put('/', function (req, res) {
+    res.status(200)
+        .json({
+        message: 'Sample PUT request'
+    });
+});
+
+router.delete('/', function (req, res) {
+    res.status(200)
+        .json({
+        message: 'Sample DELETE request'
+    });
+});
+
 
 // Export API routes
 module.exports = router;
