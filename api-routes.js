bf919d76e89a33f36e429831244ed5aea6745ce8
alexteo98 <alexteo98@gmail.com>
2022-08-25 15:18:58 +0800

    test get and post api

--- api-routes.js
+++ api-routes.js
@@ -3,10 +3,20 @@
 let router = require('express').Router();
 // Set default API response
 router.get('/', function (req, res) {
-    res.json({
-        status: 'API Its Working',
-        message: 'Welcome to RESTHub crafted with love!1'
+    res.status(200)
+    json({
+        status: 'API is Working',
+        message: 'Sample GET request'
     });
 });
+
+router.post('/', function (req, res) {
+    res.status(200)
+    .json({
+        status: 'API is Working',
+        message: 'Sample POST request'
+    });
+});
+
 // Export API routes
 module.exports = router;
